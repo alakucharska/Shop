84237e275c9c99546393abc04670ec07551e92dc
alakucharska <ala.kucharska@gmail.com>
2022-12-29 13:32:56 +0100

    Refactor after code review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  def welcome
-    render html: "Welcome to Alicja's shop!"
-    add_flash_types :danger, :info, :warning, :success, :messages
-  end
 end
