4fcdd5fbfd69166fd8363af0b8b9d771549cd999
alakucharska <ala.kucharska@gmail.com>
2023-01-01 11:50:01 +0100

    Refactor after code review

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,60 +3,55 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   let!(:product) { build(:product) }
 
-  describe "#category" do
-    it "validates presence of the category" do
+  describe " with valid parameters" do
+    it "saves product" do
       expect(product).to be_valid
+      expect { product.save! }.to change { Product.count}.by(1)
+    end
+  end
+
+  describe "with invalid parameters" do
+    describe "#name" do
+      it "validates presence" do
+        product = build(:product, name: nil)
+        expect(product).not_to be_valid
+      end
+
+      it "validates length" do
+        product = build(:product, name: "Elmo Raising"*50) 
+        expect(product).not_to be_valid
+      end
     end
 
-    context "not valid attribute" do
-      let!(:product) { build(:product, category: nil) }
+    describe "#price" do
+      it "validates presence" do
+        product = build(:product, price: nil) 
+        expect(product).not_to be_valid
+      end
 
-      it "does not save the product, if category has not been provided" do
+      it "validates value" do
+        product = build(:product, price: "dwa")
+        expect(product).not_to be_valid
+      end
+    end
+
+    describe "#description" do
+      it "validates presence" do
+        product = build(:product, description: nil) 
+        expect(product).not_to be_valid
+      end
+
+      it "validates length" do
+        product = build(:product, description: "Beautiful painting"*250) 
+        expect(product).not_to be_valid
+      end
+    end
+
+    describe "#category" do
+      it "validates presence" do
+        product = build(:product, category: nil) 
         expect(product).not_to be_valid
       end
     end
   end
-
-  describe "#name" do
-    it "validates presence of the name" do
-      expect(product).to be_valid
-    end
-
-    it "does not save the product if name has not been provided" do
-      product = build(:product, name: nil) 
-      expect(product).not_to be_valid
-    end
-
-    it "does not save the product if the name is too long" do
-      product = build(:product, name: "Elmo Raising"*50) 
-      expect(product).not_to be_valid
-    end
-  end
-
-  describe "#price" do
-    it "validates presence of the price" do
-      expect(product).to be_valid
-    end
-
-    it "does not save the product if price has not been provided" do
-      product = build(:product, price: nil) 
-      expect(product).not_to be_valid
-    end
-
-    it "does not save the product if price is not numerical" do
-      product = build(:product, price: "dwa")
-      expect(product).not_to be_valid
-    end
-  end
-
-  describe "#description" do
-    it "validates presence of the description" do
-      expect(product).to be_valid
-    end
-
-    it "does not save the product if the description has not been provided" do
-      product = build(:product, description: nil) 
-      expect(product).not_to be_valid
-    end
-  end 
 end
