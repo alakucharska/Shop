b8210dcd5ae642c6cbcdb0b5ec968357405c859a
alakucharska <ala.kucharska@gmail.com>
2023-01-03 10:20:16 +0100

    Refactor after code review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ require 'bcrypt'
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
   validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, length: { maximum: 250 }, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
