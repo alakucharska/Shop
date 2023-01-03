470ace6989800e4f861d34c04d0b1b1361d704b3
alakucharska <ala.kucharska@gmail.com>
2023-01-03 10:18:41 +0100

    Add additional validations for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "AlicjaK" }
     email { "alicjak@foobar.com" }
-    password { "Alusia89" }
+    password { "Alusia234" }
+    password_confirmation { "Alusia234" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User , type: :model do
-  let!(:user) { create(:user) }
+  let!(:user) { build(:user) }
 
   describe "with valid parameters" do
     it " saves the user" do
@@ -35,8 +35,40 @@ RSpec.describe User , type: :model do
       end
 
       it "validates uniqueness" do
+        user.save!
         user1 = User.new(name: "AlicjaK", email: "alicjak@foobar.com", password: "Alusia89")
         expect(user1).not_to be_valid
+      end
+
+      it "validates format" do
+        invalid_email_address = %w[user.example.com @USERfoo.COM A_US-ER.foo.bar.org
+                                first.last@]
+          invalid_email_address.each do |email|
+            user = build(:user, email: email)
+            expect(user).not_to be_valid
+          end
+      end
+    end
+
+    describe "#password" do
+      it "validates presence" do
+        user = build(:user, password: nil)
+        expect(user).not_to be_valid
+      end
+
+      it "validates minimum length" do
+        user = build(:user, password: "ala" )
+        expect(user).not_to be_valid
+      end
+
+      it "validates maximum length" do
+        user = build(:user, password: "ala"*15 )
+        expect(user).not_to be_valid
+      end
+
+      it "validates password confirmation" do
+        user = build(:user, password_confirmation: "ala23")
+        expect(user).not_to be_valid
       end
     end
   end
